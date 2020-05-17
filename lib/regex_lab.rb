b2795e908250925e10dbe84815bc8810510897e0
Toni  Diep <adiep123@gmail.com>
2020-05-17 23:56:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true     
+  else          
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  # binding.pry
+  text.split(" ")     
+    return text.scan(/\b\w{5}+\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    first_letter = text.scan(/\w+/)[0]
+    if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+      true
+    else  
+      false
+    end
 end
 
 def valid_phone_number?(phone)
+  phone.scan(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)!
 
 end
+  # phone.scan(/\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/)
+  # return phone
+end
+
