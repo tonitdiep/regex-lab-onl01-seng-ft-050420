26e63738bde17541e8644ef4932e41e2ce8bce99
Toni  Diep <adiep123@gmail.com>
2020-05-18 00:00:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -30,7 +30,7 @@ def valid_phone_number?(phone)
   # phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
 
 
-  phone.scan(/\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/)
+  phone.match(/\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/)
 
 end
 
